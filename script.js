bb2c449965575f986291456c0c840248abb6ebc4
IshqhsI <oneridh18@github.com>
2022-08-28 12:42:29 +0700

    Menambah File Javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let add_form = document.getElementById('add-form');
+let add_button = document.getElementById('add');
+
+add_button.addEventListener('click', function () {
+  add_form.classList.add('add-form2');
+});
