ed8a776c6e3b8b0380ed55fe238dfc74ab682d90
IshqhsI <oneridh18@github.com>
2022-08-28 23:50:46 +0700

    Ubah type input add dan tambah function di script.js

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let add_button = document.getElementById('add');
 add_button.addEventListener('click', function () {
   add_form.classList.add('add-form2');
 });
+
+var verb = document.getElementById('verb');
+let btn_add = document.getElementById('btn-add');
+
+verb.addEventListener('keyup', function () {
+  console.log(verb.value);
+});
+
+btn_add.addEventListener('click', function () {
+  key = 'iwantto' + verb.value;
+  localStorage.setItem(key, verb.value);
+  mylist = localStorage.getItem(key);
+  document.getElementById('mylist').innerHTML = mylist;
+});
